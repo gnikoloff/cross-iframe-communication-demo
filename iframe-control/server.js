a1fd8642a623619c7e6bb6203f35dc229e5c696a
Georgi Nikoloff <nikoloffgeorgi@gmail.com>
2020-06-15 21:08:09 +0200

    refactored to separate projects

---
+++ iframe-control/server.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3002
+
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/index.html`))
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
